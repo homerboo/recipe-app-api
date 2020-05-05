745275a9cef6a60aa1217aad975c7a6452fdbe5b
homerboo <mikeobw@gmail.com>
2020-05-04 20:58:30 -0400

    Added custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -7,9 +7,20 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
 
         """ Creates and saves a new user """
+        if not email:
+            raise ValueError('Users require an email')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db) # req'd to support multiple databases
+
+        return user
+
+    def create_superuser(self, email, password):
+        """ Creates and saves a new superuser """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
         return user
 
